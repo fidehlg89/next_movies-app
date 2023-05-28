6a543ae8253fc786efe322b24677e54b499324c0
Fidel Ernesto <fidehlg89@gmail.com>
2023-05-27 21:09:51 -0400

    feat: adding graphQL app

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    DATABASE_URL: "mysql://root:1234@localhost:3306/movies_db",
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
